4b337c3f58c094efa52e8a54192a69d40701df85
ShanKris <kkshanthosh@gmail.com>
2016-07-17 17:47:17 +1200

    Added rest api calls for tech skills

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,8 @@ var app = angular.module("DRMSWebApp", []);
 
 app.controller("DRMSWebController", function($scope){
   $scope.title = "DRMSWeb website";
-});
+})
+  .controller("technicalSkillsController", function($scope){    
+
+    $scope.technicalSkills = technicalSkills;
+  });
--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var app = express();
 
+var techskills = [
+      { name: "Primary Web Languages", skills: [{ name: "HTML5"}, {name: "CSS3"}, {name: "SASS/SCSS"}, {name: "JavaScript"}, {name: "Jade"}]},
+      { name: "Frontend Frameworks", skills: [{ name: "Bootstrap"}, {name: "Zurb Foundation"}, {name: "Bourbon/Neat/Refills"}, {name: "Jekyll"}]},
+      { name: "JavaScript Libraries & Templates", skills: [{ name: "Angular.js"}, {name: "jQuery"}, {name: "Webpack.js"}, {name: "Grunt.js"}, {name: "Bower.js"}, {name: "GSAP"}]},
+      { name: "Databases & Data Languages", skills: [{ name: "SQL"}, {name: "XML"}, {name: "JSON"}, {name: "MS SQL Server"}, {name: "MySql (WordPress)"}]},
+      { name: "Web Testing Tools", skills: [{ name: "Chrome Developer Tools"}, {name: "Firebug"}, {name: "jshint"}, {name: "W3C"}, {name: "Webaim Wave"}, {name: "UITest.com"}, {name: "GTMetrix"}, {name: "Pingdom"}]},
+      { name: "Web Analysis Tools", skills: [{ name: "Google Analytics"}, {name: "AdWords"}, {name: "Webmaster Tools"}, {name: "MOZ"}]},
+      { name: "Content Management Systems", skills: [{ name: "WordPress"}, {name: "Umbraco CMS"}, {name: "Joomla"}]},
+      { name: "Design Tools", skills: [{ name: "Adobe CC (Photoshop, Illustrator, InDesign, Premiere)"}]},
+      { name: "Platforms & Version Control", skills: [{ name: "Windows"}, {name: "Mac"}, {name: "Ubuntu"}, {name: "Git"}]}
+    ];
+
+    app.get('/api/techskills', function(req, res){
+      res.json(techskills);
+    });
+    app.get('/api/techskills/:index', function(req, res){
+      res.json(techskills[req.params.index]);
+    });
+
 app.use(express.static(__dirname + '/public'));
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
